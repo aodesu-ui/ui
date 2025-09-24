5577fe11761d7c0ef4cf27c4684dfd34b6f93b61
sergiocortes-dll <contacto.sergiocortes@gmail.com>
2025-09-24 13:42:51 -0500

    header updating

--- registry/aodesu/ui/button.tsx
+++ registry/aodesu/ui/button.tsx
@@ -5,30 +5,40 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*="size-"])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
+  'inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*="size-"])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
   {
     variants: {
       variant: {
-        default:
-          "bg-[hsl(var(--primary))] text-[hsl(var(--primary-foreground))]",
-        secondary:
-          "bg-[hsl(var(--secondary))] text-[hsl(var(--secondary-foreground))]",
-        outline:
-          "border border-[hsl(var(--border))] bg-transparent text-[hsl(var(--foreground))]",
-        ghost:
-          "bg-transparent text-[hsl(var(--foreground))] hover:bg-[hsl(var(--accent))]",
-        destructive:
-          "bg-[hsl(var(--destructive))] text-[hsl(var(--destructive-foreground))]",
+        ghost: {},
+        contained: {},
+        outlined: "border border-transparent"
+      },
+      color: {
+        neutral: {},
+        primary: {},
+        secondary: {},
+        contrast: {}
       },
       size: {
-        default: "h-9 px-4 py-2 has-[>svg]:px-3",
-        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+        default: "h-9 rounded-lg px-4 py-2 has-[>svg]:px-3",
         icon: "size-9",
       },
     },
+    compoundVariants: [
+      {
+        variant: "ghost",
+        color: "neutral",
+        className: "hover:bg-[var(--ghost-neutral-bg-h)]"
+      },
+      {
+        variant: "outlined",
+        color: "neutral",
+        className: "border-[hsl(var(--border-outline))]"
+      }
+    ],
     defaultVariants: {
-      variant: "default",
+      variant: "ghost",
+      color: "neutral",
       size: "default",
     },
   }
