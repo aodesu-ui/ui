2a8c81436f2d291a3464f3f8c870340787e61dcc
sergiocortes-dll <contacto.sergiocortes@gmail.com>
2025-09-26 13:58:34 -0500

    a

--- components/mdx/components.tsx
+++ components/mdx/components.tsx
@@ -5,10 +5,17 @@ export const components = {
   h1: (props: React.HTMLAttributes<HTMLHeadingElement>) => (
     <h1
       className="text-4xl font-extrabold text-blue-600 mt-6 mb-4"
-      {...props}
-    />
+      {...props} />
   ),
-
+  ul: (props: any) => (
+    <ul className="ml-4 list-disc" {...props} />
+  ),
+  ol: (props: any) => (
+    <ol className="ml-4 list-decimal" {...props} />
+  ),
+  li: (props: any) => (
+    <li className="mt-1" {...props} />
+  ),
   // wrapper para Button con código
   ButtonDemo: () => (
     <Sandbox code={`<Button>Click aquí</Button>`}>
